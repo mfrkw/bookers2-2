7ec98350ef3595c2f72d92fd3275b15e82e1bbe6
mfrkw <xjdk3766@gmail.com>
2021-09-21 11:21:12 +0000

    [ログイン後　リダイレクト先追加]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   
-  def top
-  end 
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
